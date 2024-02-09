ea82eaec87ec326544b82385dd12517de909dcc7
amaldevk <amalvasudev111@gmail.com>
2024-02-09 11:55:32 +0530

    security ok

--- controllers/blogrouter.js
+++ controllers/blogrouter.js
@@ -1,12 +1,32 @@
 const express=require("express")
 const router=express.Router()
 const blogModel=require("../model/blogmodel")
+const bcrypt=require("bcryptjs")
+
+hashPasswordGenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
 
 router.post("/signup",async(req,res)=>{
-    let data=req.body
-    let blogs=new blogModel(data)
-    let result=await blogs.save()
-    res.send({status:"success"})
-})
+    let {data}={"data":req.body}
+    let password=data.Password
+    hashPasswordGenerator(password).then(
+        (hashedPassword)=>{
+            
+            data.Password=hashedPassword
+
+            console.log(data)
+
+            let blogs=new blogModel(data)
+            let result=  blogs.save()
+
+            console.log(result)
+            res.json({status:"success"})
+        })
+        }
+    )
+
+    
 
 module.exports=router
